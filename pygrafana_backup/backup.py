079ecaad408b33b5a8a2f8e82026d0f15f71c90d
Romkeee <romkjkeeee@gmail.com>
2020-06-27 23:49:42 +0300

    Rewrote to async

--- pygrafana_backup/backup.py
+++ pygrafana_backup/backup.py
@@ -1,3 +1,6 @@
+import aiohttp
+import aiofiles
+import asyncio
 import json
 import sys
 import logging
@@ -11,17 +14,13 @@ from .config import HEADERS, SERVER, SSL_CHECK
 logger = logging.getLogger(__name__)
 
 
-def get_dashboards_uids():
+def get_dbs_uids():
     response = requests.get(f"{SERVER}/api/search?query=&type=dash-db", headers=HEADERS, verify=SSL_CHECK)
     response.raise_for_status()
     if not response.json():
         logger.info('No dashboards found to backup')
         sys.exit(0)
     return (db['uid'] for db in response.json())
-
-
-def get_dashboard_by_uid(uid):
-    return requests.get(f"{SERVER}/api/dashboards/uid/{uid}", headers=HEADERS, verify=SSL_CHECK)
 
 
 def get_current_date():
@@ -34,25 +33,28 @@ def create_sub_folder(main_folder_path):
     return sub_folder_path
 
 
-def save_dashboards(dashboards_uids, main_folder_path):
-    sub_folder_path = create_sub_folder(main_folder_path)
-    for uid in dashboards_uids:
-        db = get_dashboard_by_uid(uid).json()
+async def save_db(session, db_uid, sub_folder_path):
+    async with session.get(f"{SERVER}/api/dashboards/uid/{db_uid}", ssl=SSL_CHECK) as resp:
+        db = await resp.json()
         db_title = db['dashboard']['title']
         db_path = path.join(sub_folder_path, db_title + '.json')
-        with open(db_path, 'w') as file:
-            json.dump(db, file, indent=2)
+        async with aiofiles.open(db_path, mode='w') as file:
+            await file.write(json.dumps(db, indent=2))
         logger.info(f'Dashboard {db_title} saved to {db_path}')
 
 
-def backup(main_folder_path):
+async def backup(main_folder_path):
     try:
-        dashboards_uids = get_dashboards_uids()
-        save_dashboards(dashboards_uids, main_folder_path)
-    except requests.exceptions.HTTPError as e:
-        logger.error(f"Grafana {e.response.status_code} error during backup:\n{e.response.text}")
+        sub_folder_path = create_sub_folder(main_folder_path)
+        async with aiohttp.ClientSession(headers=HEADERS) as session:
+            await asyncio.gather(
+                *(save_db(session, db_uid, sub_folder_path) for db_uid in get_dbs_uids())
+            )
+    except aiohttp.ClientResponseError as e:
+        logger.error(f"Grafana {e.status} error during backup:\n{e.message}")
         sys.exit(1)
-    except requests.exceptions.RequestException as e:
+    except aiohttp.ClientConnectionError as e:
         logger.error(f"Error connecting to Grafana:\n{e}")
+        sys.exit(1)
     except PermissionError as e:
         logger.error(f"No permissions to create backup folder {main_folder_path}:{e}")
